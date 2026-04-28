d596c9278e2f5f7da770bdae55ada254bb4c0d7a
MuseMachine <MuseMachine@Real.com>
2026-04-29 00:48:29 +0200

    Wed 29 Apr 00:48:29 CEST 2026

---
+++ dailies/strudel/29042026.js
@@ -0,0 +1,33 @@
+/**********/
+/* SCALES */
+/**********/
+
+/*
+setcpm(60)
+$: n("0 2 4 <[6,8] [7,9]>")
+  .scale("C:minor")    // Sad, Dramatic
+  //.scale("A2:minor")   // Hopeful
+  //.scale("D:dorian")   // Happier
+  //.scale("G:mixolydian") // Lighter
+  //.scale("A2:minor:pentatonic") 
+  //.scale("F:major:pentatonic")
+  .sound("piano")
+*/
+//$: n("[22*2 20 19 18*2 <16 20>]/3").scale("C:minor").s("piano")
+
+/* With a single pattern, you get multiple different sounds*/
+/*
+$: n("<0 -3>, 2 4 <[6,8] [7,9]>")
+.scale("<C:major D:mixolydian>/4")
+.s("piano")
+*/
+
+// Elongate
+//$: n("c@3 eb").sound("gm_acoustic_bass")
+
+setcpm(60)
+$: n("<[4@2 4] [5@2 5] [6@2 6] [5@2 5]>*2")
+  .scale("<C2:mixolydian F2:mixolydian>")
+  .s("gm_acoustic_bass")
+
+$: s("bd*2, [ ~ <sd cp> ], [ ~ hh ]*2 ").bank("RolandTR909")
---
+++ mixolydian-flow.js
@@ -0,0 +1,6 @@
+setcpm(60)
+$: n("<[4@2 4] [5@2 5] [6@2 6] [5@2 5]>*2")
+  .scale("<C2:mixolydian F2:mixolydian>")
+  .s("gm_acoustic_bass")
+
+$: s("bd*2, [ ~ <sd cp> ], [ ~ hh ]*2 ").bank("RolandTR909")
